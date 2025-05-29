fb8c8e4938e2c6916caf2298e0b42175829de7eb
El Maco <marcus.myllyviita@gmail.com>
2025-05-29 20:11:56 +0300

    Main setup, TcpSocket not quite working though

--- src/bin/async_main.rs
+++ src/bin/async_main.rs
@@ -1,101 +1,270 @@
 #![no_std]
 #![no_main]
 
-use alloc::string::ToString;
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
+use embassy_net::{tcp::TcpSocket, IpAddress, IpEndpoint, Runner, StackResources};
 use esp_backtrace as _;
-use esp_hal::{clock::CpuClock, time};
-use esp_wifi::wifi::{
-    utils::create_network_interface, AccessPointInfo, AuthMethod, ClientConfiguration,
-    Configuration, WifiError, WifiStaDevice,
-};
-use log::info;
-use smoltcp::iface::SocketStorage;
+use esp_hal::clock::CpuClock;
+use esp_hal::rng::Rng;
+use esp_hal::timer::timg::TimerGroup;
+use esp_wifi::wifi::WifiStaDevice;
+use esp_wifi::{wifi::WifiDevice, EspWifiController};
+use heapless::Vec;
+use log::{debug, error, info, warn};
+use rust_mqtt::packet::v5::reason_codes::ReasonCode;
+use rust_mqtt::{client::client::MqttClient, utils::rng_generator::CountingRng};
 
 extern crate alloc;
+
+macro_rules! mk_static {
+    ($t:ty,$val:expr) => {{
+        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+        #[deny(unused_attributes)]
+        let x = STATIC_CELL.uninit().write(($val));
+        x
+    }};
+}
+
+const SSID: &str = env!("SSID");
+const PASS: &str = env!("PASSWORD");
+
+const MQTT_HOST: &str = env!("MQTT_HOST");
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
     // generator version: 0.2.2
 
-    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let config: esp_hal::Config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
     esp_alloc::heap_allocator!(72 * 1024);
 
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let mut rng = Rng::new(peripherals.RNG);
+
     esp_println::logger::init_logger_from_env();
 
-    let timer0 = esp_hal::timer::systimer::SystemTimer::new(peripherals.SYSTIMER);
-    esp_hal_embassy::init(timer0.alarm0);
-
-    info!("Embassy initialized!");
+    // let timer0 = esp_hal::timer::systimer::SystemTimer::new(peripherals.SYSTIMER);
 
     // configure wifi
-    let timg0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0);
-    let init = esp_wifi::init(
-        timg0.timer0,
-        esp_hal::rng::Rng::new(peripherals.RNG),
-        peripherals.RADIO_CLK,
-    )
-    .unwrap();
+    let init = &*mk_static!(
+        EspWifiController<'static>,
+        esp_wifi::init(timg0.timer0, rng.clone(), peripherals.RADIO_CLK).unwrap()
+    );
 
-    let mut wifi = peripherals.WIFI;
-    let mut socket_set_entries: [SocketStorage; 3] = Default::default();
+    let (wifi_device, wifi_controller) =
+        esp_wifi::wifi::new_with_mode(&init, peripherals.WIFI, WifiStaDevice).unwrap();
 
-    let (iface, device, mut controller) =
-        create_network_interface(&init, &mut wifi, WifiStaDevice).unwrap();
+    esp_hal_embassy::init(timg0.timer1);
+    info!("Embassy initialized!");
 
-    let ssid = env!("SSID");
-    let pass = env!("PASSWORD");
-    let client_config = Configuration::Client(ClientConfiguration {
-        ssid: heapless::String::try_from("todo").unwrap(),
-        bssid: None,
-        auth_method: AuthMethod::WPAWPA2Personal,
-        password: heapless::String::try_from("todo").unwrap(),
-        channel: None,
-    });
+    let mut config = embassy_net::Config::dhcpv4(Default::default());
+    config.ipv6 = embassy_net::ConfigV6::None;
+    let seed = (rng.random() as u64) << 32 | rng.random() as u64;
 
-    let res = controller.set_configuration(&client_config);
-    info!("Wi-Fi set_configuration returned {:?}", res);
+    let (stack, runner) = embassy_net::new(
+        wifi_device,
+        config,
+        mk_static!(StackResources<3>, StackResources::<3>::new()),
+        seed,
+    );
 
-    controller.start().unwrap();
-    info!("Is wifi started: {:?}", controller.is_started());
+    spawner.spawn(connection(wifi_controller)).ok();
+    spawner.spawn(net_task(runner)).ok();
 
-    info!("Start Wifi Scan");
-    let res: Result<(heapless::Vec<AccessPointInfo, 10>, usize), WifiError> = controller.scan_n();
-    if let Ok((res, _count)) = res {
-        for ap in res {
-            info!("{:?}", ap);
-        }
+    info!("Waiting for network to be ready...");
+    stack.wait_config_up().await;
+
+    info!("Waiting to get IP address...");
+    if let Some(config) = stack.config_v4() {
+        info!("Network configuration:");
+        info!("    IP: {}", config.address);
+        info!("    Gateway: {:?}", config.gateway);
+        info!("    DNS servers: {:?}", config.dns_servers);
+    } else {
+        error!("No IPv4 configuration available!");
     }
 
-    info!("{:?}", controller.capabilities());
-    info!("Wi-Fi connect: {:?}", controller.connect());
-
-    info!("Wait to get connected");
     loop {
-        let res = controller.is_connected();
-        match res {
-            Ok(connected) => {
-                if connected {
-                    info!("Wi-Fi is CONNECTED!");
-                    break;
+        sleep(1_000).await;
+        let mut rx_buffer = [0; 4096];
+        let mut tx_buffer = [0; 4096];
+
+        let mut socket: TcpSocket<'_> = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
+
+        socket.set_timeout(Some(embassy_time::Duration::from_secs(60)));
+
+        let ip = str_to_ip(MQTT_HOST);
+        let port = 1883;
+        let remote_endpoint = IpEndpoint::new(ip, port);
+        info!("Attempting to connect to {}:{}", ip, port);
+        let connection = socket.connect(remote_endpoint).await;
+        warn!("connection: {:?}", connection);
+        match connection {
+            Ok(()) => {
+                info!("✓ Successfully connected to {}:{}", ip, port);
+
+                // Try to send a simple byte to test the connection
+                match socket.write(&[0x10, 0x00]).await {
+                    Ok(bytes_written) => {
+                        info!("✓ Successfully wrote {} bytes", bytes_written);
+                    }
+                    Err(e) => {
+                        error!("✗ Failed to write data: {:?}", e);
+                    }
+                }
+
+                // Try to read response (MQTT CONNACK or connection close)
+                let mut buf = [0; 64];
+                socket.set_timeout(Some(embassy_time::Duration::from_secs(10)));
+                match socket.read(&mut buf).await {
+                    Ok(bytes_read) => {
+                        info!("✓ Read {} bytes: {:?}", bytes_read, &buf[..bytes_read]);
+                    }
+                    Err(e) => {
+                        warn!("Could not read response (this might be normal): {:?}", e);
+                    }
+                }
+
+                socket.close();
+                info!("Connection test completed successfully");
+                break;
+            }
+            Err(e) => {
+                error!("✗ Failed to connect: {:?}", e);
+
+                // Additional debugging information
+                if let Some(config) = stack.config_v4() {
+                    info!("Current network config still valid:");
+                    info!("  Our IP: {}", config.address.address());
+                    info!("  Gateway: {:?}", config.gateway);
+
+                    // Check if we're trying to connect to something on our subnet
+                    let our_ip = config.address.address().octets();
+                    let target_ip: Vec<&str, 4> = MQTT_HOST.split('.').collect();
+                    let target_ip: Vec<u8, 4> =
+                        target_ip.iter().map(|a| a.parse().unwrap_or(0)).collect();
+                    let subnet_mask = config.address.prefix_len();
+
+                    info!("Network analysis:");
+                    info!(
+                        "  Our IP: {}.{}.{}.{}/{}",
+                        our_ip[0], our_ip[1], our_ip[2], our_ip[3], subnet_mask
+                    );
+                    info!(
+                        "  Target IP: {}.{}.{}.{}",
+                        target_ip[0], target_ip[1], target_ip[2], target_ip[3]
+                    );
+
+                    // Simple same-subnet check (assuming /24 network)
+                    if our_ip[0] == target_ip[0]
+                        && our_ip[1] == target_ip[1]
+                        && our_ip[2] == target_ip[2]
+                    {
+                        info!("  ✓ Target appears to be on same subnet");
+                    } else {
+                        info!("  ! Target appears to be on different subnet - routing through gateway");
+                    }
+                    continue;
+                } else {
+                    error!("Network configuration lost!");
                 }
             }
-            Err(err) => {
-                info!("{:?}", err);
-                loop {}
+        }
+        info!("connected!");
+
+        let mut config = rust_mqtt::client::client_config::ClientConfig::new(
+            rust_mqtt::client::client_config::MqttVersion::MQTTv5,
+            CountingRng(20000),
+        );
+        config.add_max_subscribe_qos(rust_mqtt::packet::v5::publish_packet::QualityOfService::QoS1);
+        config.add_client_id("sven");
+        config.max_packet_size = 100;
+        let mut recv_buffer = [0; 80];
+        let mut write_buffer = [0; 80];
+
+        let mut client =
+            MqttClient::<_, 5, _>::new(socket, &mut write_buffer, 80, &mut recv_buffer, 80, config);
+
+        match client.connect_to_broker().await {
+            Ok(()) => {}
+            Err(mqtt_error) => match mqtt_error {
+                ReasonCode::NetworkError => {
+                    error!("MQTT Network Error: {:?}", mqtt_error);
+                    continue;
+                }
+                _ => {
+                    error!("Other MQTT Error: {:?}", mqtt_error);
+                    continue;
+                }
+            },
+        }
+    }
+
+    //////////////////////////
+    // MQTT:
+    // refer to https://github.com/JurajSadel/esp32s3-no-std-async-mqtt-demo/blob/main/src/main.rs
+    //////////////////////////
+    // Refer to https://github.com/JurajSadel/esp32s3-no-std-async-mqtt-demo
+    // Refer to https://github.com/esp-rs/esp-wifi-sys/blob/68dc11bbb2c0efa29c4acbbf134d6f142441065e/examples-esp32c3/examples/embassy_dhcp.rs
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.23.1/examples/src/bin
+}
+
+#[embassy_executor::task]
+async fn connection(mut controller: esp_wifi::wifi::WifiController<'static>) {
+    info!("start connection task");
+    debug!("Device capabilities: {:?}", controller.capabilities());
+    loop {
+        match esp_wifi::wifi::wifi_state() {
+            esp_wifi::wifi::WifiState::StaConnected => {
+                // wait until we're no longer connected
+                controller
+                    .wait_for_event(esp_wifi::wifi::WifiEvent::StaDisconnected)
+                    .await;
+                sleep(5000).await
+            }
+            _ => {}
+        }
+        if !matches!(controller.is_started(), Ok(true)) {
+            let client_config =
+                esp_wifi::wifi::Configuration::Client(esp_wifi::wifi::ClientConfiguration {
+                    ssid: SSID.try_into().unwrap(),
+                    password: PASS.try_into().unwrap(),
+                    ..Default::default()
+                });
+            controller.set_configuration(&client_config).unwrap();
+            info!("Starting wifi");
+            controller.start_async().await.unwrap();
+            info!("Wifi started!");
+        }
+        info!("About to connect...");
+
+        match controller.connect_async().await {
+            Ok(_) => info!("Wifi connected!"),
+            Err(e) => {
+                error!("Failed to connect to wifi: {e:?}");
+                sleep(5000).await
             }
         }
     }
-    // TODO: Spawn some tasks
-    let _ = spawner;
+}
 
-    loop {
-        info!("Hello world!");
-        Timer::after(Duration::from_secs(1)).await;
-    }
+#[embassy_executor::task]
+async fn net_task(mut runner: Runner<'static, WifiDevice<'static, esp_wifi::wifi::WifiStaDevice>>) {
+    runner.run().await
+}
 
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.23.1/examples/src/bin
+pub async fn sleep(millis: u32) {
+    embassy_time::Timer::after(embassy_time::Duration::from_millis(millis as u64)).await;
+}
+
+fn str_to_ip(ip: &str) -> IpAddress {
+    let split_ip: Vec<&str, 4> = ip.split('.').collect();
+    IpAddress::v4(
+        split_ip[0].parse().unwrap_or(0),
+        split_ip[1].parse().unwrap_or(0),
+        split_ip[2].parse().unwrap_or(0),
+        split_ip[3].parse().unwrap_or(0),
+    )
 }
